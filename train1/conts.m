19a95f44191c38d14193f67d92affa35621731f7
KYankova <kv_yankova@yahoo.com>
2015-02-21 02:25:20 +0000

    Janitorial work: removing commented out code, documenting code and stilysing code

--- train1/conts.m
+++ train1/conts.m
@@ -1,101 +1,83 @@
 
 function conts = countours(img)
 %{
-   --This function takes in a binary
+   -- This function takes in a binary
    image and proceeds to find regions
    within such image.
    -- The thresholding/edge detection
    is left to be done prior to this 
    function.
-   -- The pause(n) are just there
-   for debugging purposes atm.
 %}
 
-% Morphological transformation
-% removes open 1 valued pixels
-% for a given specified size
-% bw = bwareaopen(img,1);
-bw = img;
 % dimensions
 [height width] = size(img);
-% [height width]
-% display  binary image
-imshow(bw)
+
+% display image
+imshow(img)
 
 % lable connecteded elements
-[lable na]=bwlabel(bw);
+[lable na] = bwlabel(img);
 
 % calculate the properties of the regions
 % within the image
-property= regionprops(lable);
+property = regionprops(lable);
 
-%NOT USED. Center of mass calculation
-[y x] = find( bw);
+% TO DO. Center of mass calculation
+[y x] = find(img);
 cent = [mean(x) mean(y)];
 
-% Retain current plots when adding 
-% new plots
-
+% Retain current plots when adding new plots
 hold on
 
-% [maxs row] = max([property.Area], [],2);
-% main_box = property(row).BoundingBox;
-% tester = zeros(height,width);
-% for i=1:height
-%     for j=1:width
-%         if j > main_box(1) & j < main_box(3) +  main_box(1)...
-%         & i > main_box(2) & i < main_box(4) + main_box(2)
-%             tester(i,j) = bw(i,j);
-%             disp('HELP');
-%         end
-%     end
-% end
-% imshow(tester);
-% disp(main_box);
 % draw the bounding boxes in the image
 [none asize] = size([property.Area]);
 area = zeros(none, asize);
-for n=1:size(property,1)
 
-    rectangle('Position',property(n).BoundingBox,'EdgeColor','g','LineWidth',2);
+for n=1:size(property, 1)
+
+    rectangle('Position', ...
+              property(n).BoundingBox, ...
+              'EdgeColor', ...
+              'g', ...
+              'LineWidth', ...
+              2);
     area(n) = property(n).BoundingBox(3) * property(n).BoundingBox(4);
-
 end
-
-% pause (3)
 
 % find areas smaller than 200
-
 small_areas=find([property.Area]<200);
 
-% display areas (in red)
-% smaller than 200
+% display areas (in red) smaller than 200
 for n=1:size(small_areas,2)
 
-    rectangle('Position',...
-    	       property(small_areas(n)).BoundingBox, ...
-    		   'EdgeColor','r','LineWidth',2);
-
+    rectangle('Position', ...
+    	        property(small_areas(n)).BoundingBox, ...
+    		      'EdgeColor', ...
+              'r', ...
+              'LineWidth', ...
+              2);
 end
 
-% pause (2)
-
-% remove the small areas.
+% remove the small areas
 for n=1:size(small_areas,2)
 
     coord=round(property(small_areas(n)).BoundingBox);
 
-
-    bw(coord(2):coord(2)+coord(4),...
-       coord(1):coord(1)+coord(3))=0;
-
+    img(coord(2): coord(2) + coord(4), coord(1): coord(1) + coord(3)) = 0;
 end
 
 figure
-imshow(bw)
+imshow(img)
 
-out.prop = property
-out.bw = bw
+% array structure 'out' containing:
+
+% default region properties
+out.prop = property;
+
+% noise cleaned binary image
+out.bw = img;
+
+% areas of BoundingBoxes
 out.boxarea = area;
-%return region properties (along with bounding boxes)
+
 conts = out;
--- train1/run.m
+++ train1/run.m
@@ -1,101 +1,41 @@
 img = imread('../train1/train8.jpg');
-% dimensions of the image
-% [height width channels] = size(img);
 
 I = rgb2gray(img);
 
-% J = imnoise(img,'gaussian',0.02);
+% laplacian edge detection
+threshed = edge(I, 'log');
 
-% imshow(J);
-
-% J = img;
-
-% K = filter2(fspecial('average',10),J)/255;
-
-% imshow(J);
-
-% L = medfilt2(J,[3 3]);
-
-% imshow(L);
-
-
-% normalize rgb
-% normed = rgbnorm(img);
-% normed = img;
-
-% imshow(normed);
-
-% threshold normalized rgb image
-% HERE BE BABY
-% threshed = thresh_norm(img);
-
-threshed = edge(I,'log');
-
-size(threshed)
-% draw contours for regions in
-% thresholded image
-
-
-% flatten values in redscale the two
-% last parameters of reshape
-% define the new dimensions of vecr
-% vecr = reshape(normed(:,:,1),1,width*height);
-% % same for green
-% vecg = reshape(normed(:,:,2),1,width*height);
-% % same for blue
-% vecb = reshape(normed(:,:,3),1,width*height);
-% % plot histograms
-% hist(vecr,1000)
-% hist(vecr,1000)
+% bounding box for card
 out1 = conts(threshed);
 property = out1.prop;
-[maxs row] = max([property.Area], [],2);
+[maxs row] = max([property.Area], [], 2);
 main_box = property(row).BoundingBox;
 tester1 = imcrop(img, main_box);
 
-% filter1 = fspecial('gaussian', [2,2], 6);
-% tester1 = imfilter(tester1, filter1);
-% tester1 = bwmorph(tester1, 'open', 1);
-% figure
-% imshow(threshed)
-
-% property.BoundingBox
-
+% converting to grayscale
 tester = rgb2gray(tester1);
-threshed2 = ~ im2bw(tester,graythresh(tester));
 
+% binary threshold of grayscale image
+threshed2 = ~ im2bw(tester, graythresh(tester));
 
+% noise removal via open
 threshed2 = bwmorph(threshed2, 'open', 1);
-% threshed2 = bwareaopen(threshed2, 20);
 
-% black = thresher(tester,100,20);
+% noise removal within the card region
 out2 = conts(threshed2);
 bwf = out2.bw;
-% for i=1:height
-%     for j=1:width
-%         if j > main_box(1) & j < main_box(3) +  main_box(1)...
-%         & i > main_box(2) & i < main_box(4) + main_box(2)
-%             % tester(i,j,1) = img(i,j,1);
-%             % tester(i,j,2) = img(i,j,2);
-%             % tester(i,j,3) = img(i,j,3);
-%             disp('HELP');
-%         else
-%         	tester(i,j,1) = 0;
-%             tester(i,j,2) = 0;
-%             tester(i,j,3) = 0;
-%         end
-%     end
-% end
+
+% detection of suits and number
 out3 = conts(bwf);
 property3 = out3.prop;
-[mins row3] = min(out3.boxarea, [],2);
+[mins row3] = min(out3.boxarea, [], 2);
 smallest = property3(row3).BoundingBox;
 smallest = imcrop(tester1, smallest);
 
 figure
 imshow(smallest);
 
-[maxes row3m] = max(out3.boxarea, [],2);
+[maxes row3m] = max(out3.boxarea, [], 2);
 biggest = property3(row3m).BoundingBox;
 biggest = imcrop(tester1, biggest);
 
